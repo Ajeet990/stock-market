cf37678bf8a4a31e51e74b4e4e85bce91e10ab65
Ajeet990 <70121854+Ajeet990@users.noreply.github.com>
2021-10-03 16:12:43 +0545

    Update userController.js
    commited

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,10 +1,11 @@
 const mysql = require("mysql");
 
 //connectio pool
-const pool = mysql.createPool({connectionLimit: 100, host: process.env.DB_HOST, user: process.env.DB_USER, password: process.env.DB_PASS, database: process.env.DB_NAME});
+// const pool = mysql.createPool({connectionLimit: 100, host: process.env.DB_HOST, user: process.env.DB_USER, password: process.env.DB_PASS, database: process.env.DB_NAME});
 
 //view users
 exports.view = (req, res) => {
+  res.render('home')
   //Route
   //connect to db
   pool.getConnection((err, connection) => {
